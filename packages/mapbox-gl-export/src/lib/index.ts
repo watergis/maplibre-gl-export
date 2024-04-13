dde113749c3b3948bd7823d4f4cfb29c65ce4e15
Jin Igarashi <igarashi.jin@water-gis.com>
2024-04-13 10:48:16 +0900

    ci: changed to node v20

--- packages/mapbox-gl-export/src/lib/index.ts
+++ packages/mapbox-gl-export/src/lib/index.ts
@@ -1,3 +1,3 @@
 export { default as MapboxExportControl } from './export-control';
 export { Size, PageOrientation, Format, DPI } from '@watergis/maplibre-gl-export';
-export * from './interfaces'
+export * from './interfaces';
