d30043fd620a2d0feb911080c6e3a765aacd1f80
Jin Igarashi <igarashi.jin@water-gis.com>
2024-04-14 09:50:12 +0900

    added html for testing cdn (#113)
    * added html for testing cdn
    
    * disable eslint

---
+++ packages/mapbox-gl-export/index_cdn.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef*/
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESSTOKEN;
+const map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [35.87063, -1.08551],
+	zoom: 9
+});
+
+map.addControl(
+	new MapboxExportControl.MapboxExportControl({
+		PageSize: MapboxExportControl.Size.A4,
+		PageOrientation: MapboxExportControl.PageOrientation.Landscape,
+		Format: MapboxExportControl.Format.PNG,
+		DPI: MapboxExportControl.DPI[300],
+		Crosshair: true,
+		PrintableArea: true,
+		Local: 'fr',
+		accessToken: mapboxgl.accessToken
+	}),
+	'top-right'
+);
