b48767b49c0be26aec277851426474affa9a43c7
Jin Igarashi <igarashi.jin@water-gis.com>
2024-04-29 08:53:34 +0900

    refactor: export AvailableLanguages as an array from mapblire-gl-export

---
+++ .changeset/unlucky-otters-unite.md
@@ -0,0 +1,6 @@
+---
+"@watergis/maplibre-gl-export": patch
+"@watergis/mapbox-gl-export": patch
+---
+
+refactor: export AvailableLanguages as an array from mapblire-gl-export
--- sites/demo/src/lib/LanguageSelector.svelte
+++ sites/demo/src/lib/LanguageSelector.svelte
@@ -36,7 +36,7 @@
 </script>
 
 <script lang="ts">
-	import { type Language, getTranslation } from '@watergis/maplibre-gl-export';
+	import { type Language, getTranslation, AvailableLanguages } from '@watergis/maplibre-gl-export';
 	import type { Map as MapboxMap } from 'mapbox-gl';
 	import type { Map as MaplibreMap } from 'maplibre-gl';
 	import { createEventDispatcher, onDestroy, onMount } from 'svelte';
@@ -49,19 +49,6 @@
 
 	let control: LanguageControl | undefined;
 	let controlGroup: HTMLDivElement;
-	let languages: Language[] = [
-		'en',
-		'fr',
-		'fi',
-		'de',
-		'sv',
-		'es',
-		'vi',
-		'uk',
-		'zhHans',
-		'zhHant',
-		'ja'
-	];
 
 	const handleClickLanguage = (lang: Language) => {
 		language = lang;
@@ -87,7 +74,7 @@
 	class="maplibregl-ctrl maplibregl-ctrl-group mapboxgl-ctrl mapboxgl-ctrl-group"
 	bind:this={controlGroup}
 >
-	{#each languages as lang}
+	{#each AvailableLanguages as lang}
 		<button
 			class="lang-button {language === lang ? 'selected' : ''}"
 			on:click={() => {
