64d39b08982b755ba3fc3bb907459968bec90385
Jin Igarashi <igarashi.jin@water-gis.com>
2024-04-29 09:00:26 +0900

    refactor: export AvailableLanguages as an array from mapblire-gl-export (#136)

--- packages/maplibre-gl-export/src/lib/interfaces/Language.ts
+++ packages/maplibre-gl-export/src/lib/interfaces/Language.ts
@@ -1,12 +1,15 @@
-export type Language =
-	| 'de'
-	| 'en'
-	| 'fr'
-	| 'fi'
-	| 'sv'
-	| 'es'
-	| 'vi'
-	| 'uk'
-	| 'zhHans'
-	| 'zhHant'
-	| 'ja';
+export const AvailableLanguages = [
+	'en',
+	'fr',
+	'fi',
+	'de',
+	'sv',
+	'es',
+	'vi',
+	'uk',
+	'zhHans',
+	'zhHant',
+	'ja'
+] as const;
+
+export type Language = (typeof AvailableLanguages)[number];
