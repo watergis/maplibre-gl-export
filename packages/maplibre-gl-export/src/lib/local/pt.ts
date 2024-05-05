6b9204d959c437364edbf4dd556d6ce218abe02e
Leonel Dias <leoneldias@triedeti.pt>
2024-05-05 02:13:06 +0100

    Create pt.ts (#133)

---
+++ packages/maplibre-gl-export/src/lib/local/pt.ts
@@ -0,0 +1,12 @@
+import { Translation } from '../interfaces/Translation';
+
+const translation: Translation = {
+	PageSize: 'Tamanho da página',
+	PageOrientation: 'Orientação da página',
+	Format: 'Formato',
+	DPI: 'DPI',
+	Generate: 'Gerar',
+	LanguageName: 'Português'
+};
+
+export default translation;
