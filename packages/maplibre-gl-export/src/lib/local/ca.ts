05f843a6d7f44c0bf0a0168bd97023ab583d06dc
L.Struth <60439791+lstiz@users.noreply.github.com>
2024-05-08 09:52:32 +0200

    Create ca.ts
    Catalan translation

---
+++ packages/maplibre-gl-export/src/lib/local/ca.ts
@@ -0,0 +1,13 @@
+import { Translation } from '../interfaces/Translation';
+
+const translation: Translation = {
+	PageSize: 'Mida',
+	PageOrientation: 'Orientació',
+	Format: 'Format',
+	DPI: 'DPI',
+	Generate: 'Genera',
+	LanguageName: 'Catalan',
+	LanguageCode: 'ca'
+};
+
+export default translation;
