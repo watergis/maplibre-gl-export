0f744409b98f6e18ab7348771c5e21c715acee9a
Hans Christian Diekmann <cdiekmann33@gmail.com>
2025-05-18 14:58:20 +0200

    adds italian locale translations

---
+++ packages/maplibre-gl-export/src/lib/local/it.ts
@@ -0,0 +1,13 @@
+import { Translation } from '../interfaces/Translation';
+
+const translation: Translation = {
+	PageSize: 'Formato carta',
+	PageOrientation: 'Orientamento carta',
+	Format: 'Formato file',
+	DPI: 'Risoluzione di stampa',
+	Generate: 'Genera',
+	LanguageName: 'Italiano',
+	LanguageCode: 'it'
+};
+
+export default translation;
